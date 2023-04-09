546dd57ad53375112e79cc5ffbfd05dadb948fef
Valais Girl <Yevette@Hal-71.local>
2023-04-09 12:41:51 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// 1. Why is a breed-up program necessary in the USA for the Valais Blacknosed Sheep?
+    // a. The Valais Sheep has gone extinct
+    // b. The USDA does not currently permit the import of live sheep from other countries
+    // c. The Valais Sheep is a new hybrid species developed for consumption that will rival 'The impossible burger'
+
+// 2. Which of the following breeds can be used as a foundation in the breed-up program?
+    // a. The Horned Dorset
+    // b. The Scottish Blackface
+    // c. The Lincolnshire 
+    // d. All of the Above
+
+// 3. Where did the Valais breed originate?
+    // a. Germany
+    // b. Russian
+    // c. Switzerland
+    // d. Italy
+
+// 4. How many generations of breeding-up does it take to have a purebred Valais sheep?
+    // a. 5 generations
+    // b. 7 generations
+    // c. 3 generations
+    // d. it is not possible to get a purebred sheep from a breed-up program.
+
+// 5. What is the cutest sheep in the world?
+    // a. The Valais, of course!
+    // b. Have you seen the Valais? Definitely the cutest!
+    // c. Seriously, have you seen a Valais? No contest!
+    // d. All of the above
+
+
+
+
+
+    // ## Acceptance Criteria
+
+// ```
+// GIVEN I am taking a code quiz
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and my score
+// ```
