370539774495cac3543479422ebb26ffb54cd30a
Valais Girl <Yevette@Hal-71.local>
2023-04-13 09:29:51 -0500

    Rewrite Questions

--- script.js
+++ script.js
@@ -1,130 +1,211 @@
-var startButton = document.getElementById('start-btn')
-var nextButton = document.getElementById('next-btn')
-var questionContainerElement = document.getElementById('quiz-container')
-var questionElement = document.getElementById('question')
-var answerButtonsElement = document.getElementById('answer-buttons')
-
-let currentQuestionIndex
-
-startButton.addEventListener('click', startQuiz)
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-})
-
-console.log('lets begin')
-function startQuiz(){
-    console.log('Lets Begin')
-    startButton.classList.add('hide')
-    // shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
-
-}
-
-function setNextQuestion(){
-    resetState()
-//  showQuestion(shuffledQuestions[currentQuestionIndex])
-}
 
 
-function showQuestion(question) {
-    questionElement.innerText = question.question
-    question.answers.forEach(answer => {
-            var button = document.createElement('button')
-            button.innerText = answer.text
-            button.classList.add('btn')
-            if (answer.correct) {
-                button.dataset.correct = answer.correct
-            }
-            button.addEventListener('click', selectAnswer)
-            answerButtonsElement.appendChild(button)
-    })
-}
-function resetState(){
-    nextButton.classList.add('hide')
-    while (answerButtonsElement.firstChild) {
-        // answerButtonsElement.removeChild
-        (answerButtonsElement.firstchild)
-    }
-}
-function selectAnswer(e){
-    var selectedButton = e.target
-    var correct = selectedButton.dataset.correct
-    setStatusClass(document.body, correct)
-    Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button, button.dataset.correct)
-    })
-    nextButton.classList.remove('hide')
+const startButton = document.getElementById
+('start-btn')
+// const nextButton = document.getElementById('next-btn')
+// const questionContainerElement = document.getElementById('question-container')
+// const questionElement = document.getElementById('question')
+// const answerButtonsElement = document.getElementById('answer-buttons')
+// let shuffledQuestions, currentQuestionIndex
+// let currentQuestionIndex = 0;
 
-}
+// startButton.addEventListener('click', startQuiz)
 
-function setStatusClass(element, correct) {
-    clearsStatusClass(element)
-    if (correct) {
-        element.classList.add ('correct')
-    }   else {
-        element.classList.add ('wrong')
-    }
-}
+// nextButton.addEventListener('click', () => {
+    // currentQuestion++
+// })
 
-var questions = [
+// function startQuiz() {
+    // currentQuestionIndex = 0;
+    // nextButton.innerText = "Next";
+    // showQuestion();
+    // startButton.classList.add('hide');
+    // currentQuestionIndex = 0;
+    // questionElement.innerText = question[currentQuestion].question;
+    // answerButtonsElement.innerText = questions[currentQuestion].answers[0].option;
+    // answerButtonsElement
+    // questionContainerElement.classList.remove('hide');
+    // if (currentQuestion < 4) {
+    // setNextQuestion()
+    // }
+// }
+
+// function showQuestion() {
+    // resetState();
+    // let currentQuestionIndex = questions[currentQuestionIndex];
+    // let questionNo = currentQuestionIndex + 1;
+    // questionElement.innerText = questionNo + "." + currentQuestion.question;
+
+    // currentQuestion.answers.forEach(answer => {
+        // const button = document.createElement("button");
+        // button.innerText = answer.text;
+        // button.classList.add("btn");
+        // answerButtonsElement.appendChild(button);
+    // });
+// }
+
+// function resetState(){
+    // nextButton.style.display = "none";
+    // while(answerButtons.firstChild){
+        // answerButtons.removeChild(answerButtons.firstchild);
+    // }
+
+// }
+// 
+// startQuiz();
+
+// function setNextQuestion(){
+    // currentQuestion++;
+    // if(currentQuestion >= 4) {
+    //  nextButton.classList.add('hide')
+    // }
+    // resetState()
+    // showQuestion(shuffledQuestions[currentQuestionIndex])
+// }
+
+
+// function showQuestion(question) {
+    // let currentQuestionIndex = questions[currentQuestion];
+    // let questionNo = currentQuestion + 1;
+    // questionElement.innerText = questionNo + "." + currentQuestion.question;
+    // currentQuestion.answers.forEach(answer => {
+            // const button = document.createElement('button');
+            // button.innerText = answer.text;
+            // button.classList.add('btn');
+            // if (answer.correct) {
+                // button.dataset.correct = answer.correct
+            // }
+            // button.addEventListener('click', selectAnswer)
+            // answerButtonsElement.appendChild(button)
+    // })
+// }
+// function resetState(){
+//     nextButton.classList.add('hide')
+//     while (answerButtonsElement.firstChild) {
+//         // answerButtonsElement.removeChild
+//         (answerButtonsElement.firstchild)
+//     }
+// }
+// function selectAnswer(e){
+//     const selectedButton = e.target
+//     const correct = selectedButton.dataset.correct
+//     setStatusClass(document.body, correct)
+//     Array.from(answerButtonsElement.children).forEach(button => {
+//         setStatusClass(button, button.dataset.correct)
+//     })
+//     if (shuffledQuestions.length > currentQuestionIndex + 1) {
+//     nextButton.classList.remove('hide')
+//     } else { 
+//         startButton.innerText = 'Restart'
+//         startButton.classList.remove('hide')
+//     }
+// }
+
+// function setStatusClass(element, correct) {
+//     clearsStatusClass(element)
+//     if (correct) {
+//         element.classList.add ('correct')
+//     }   else {
+//         element.classList.add ('wrong')
+//     }
+// }
+// function clearStatusClass(element) {
+//     element.classList.remove('correct')
+//     element.classList.remove('wrong')
+// }
+// let currentQuestion = 0;
+const questions = [
     {
-        question: 'Why is a breed-up program necessary in the USA for the Valais Blacknosed Sheep?',
-        answers: [
-            {text: 'The Valais sheep has gone extinct', correct: false },
-            {text: 'The USDA does not currently permit the import of live sheep from other countries', correct: true },
-            {text: 'The Valais sheep is a new hybrid species developed for consumption that will rival GMO impossible burger meat', correct: false},
-            {text: 'You can never have enough sheep', correct: false}
-        ]
+        text: 'Why is a breed-up program necessary in the USA for the Valais Blacknosed Sheep?',
+        choices:[ 'The Valais sheep has gone extinct', 'The USDA does not currently permit the import of live sheep from other countries', 'The Valais sheep is a new hybrid species developed for consumption, that will rival GMO impossible burger meat','You can never have enough sheep'],
+        answerIndex: 1
+    },
+
+    {
+        text: 'Which of the following breeds can be used as a foundation in the breed-up program?',
+        choices:['Horned Dorset', 'Scottish Blackface', 'Teeswater','All of the Above'],
+        answerIndex: 3  
+
+        
+    },
+
+    {
+        question: 'Where did the Valais breed originate?',
+        choices: ['Germany', 'Russia','Switzerland','Italy'],
+        answerIndex: 2
+
+    },
+    {
+        question: 'How many generations does it take to have a purebred Valais sheep?',
+        choices: ['5 generations', '7 generations','3 generations','it is not possible to get a purebred sheep from a breed-up program'],
+        answerIndex: 0
+    },
+    {
+        question: 'Both ewes and rams are horned in the Valais',
+        choices: [ 'true','false',],
+        answerIndex: 0
+        
     }
+ ]
+  
+  
+   
+
+   
 ]
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
-// 1. Why is a breed-up program necessary in the USA for the Valais Blacknosed Sheep?
-    // a. The Valais Sheep has gone extinct
-    // b. The USDA does not currently permit the import of live sheep from other countries
-    // c. The Valais Sheep is a new hybrid species developed for consumption that will rival 'The impossible burger'
+let currentQuestionIndex = 0;
 
-// 2. Which of the following breeds can be used as a foundation in the breed-up program?
-    // a. The Horned Dorset
-    // b. The Scottish Blackface
-    // c. The Lincolnshire 
-    // d. All of the Above
+function startQuiz(){
+    currentQuestionIndex =0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
 
-// 3. Where did the Valais breed originate?
-    // a. Germany
-    // b. Russian
-    // c. Switzerland
-    // d. Italy
+}
 
-// 4. How many generations of breeding-up does it take to have a purebred Valais sheep?
-    // a. 5 generations
-    // b. 7 generations
-    // c. 3 generations
-    // d. it is not possible to get a purebred sheep from a breed-up program.
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
 
-// 5. What is the cutest sheep in the world?
-    // a. The Valais, of course!
-    // b. Have you seen the Valais? Definitely the cutest!
-    // c. Seriously, have you seen a Valais? No contest!
-    // d. All of the above
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild.removeChild(answerButtons.firstChild));
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect) {
+        selectedBtn.classList.add("correct");
+    } else{
+        selectAnswerBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+startQuiz();
 
 
-
-
-
-    // ## Acceptance Criteria
-
-// ```
-// GIVEN I am taking a code quiz
-// WHEN I click the start button
-// THEN a timer starts and I am presented with a question
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and my score
-// ```
